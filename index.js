33a0029833bae46f7d10cf533d00bd3ccd535604
Basil Haidi <basilhaidi65@gmail.com>
2022-05-29 19:58:21 +0700

    menambahkan array colorFirstUser, colorSecondUser, firstUserRestaurant, secondUserRestaurant; menambahkan set uniqueFirstUser, uniqueSecondUser, uniqueColorFirst, uniqueColorSecond; menambahkan objek firstUser dan secondUser

--- index.js
+++ index.js
@@ -1,20 +1,109 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+// Basil Haidi Farizan
+
+const colorFirstUser = ['Yellow', 'Pink', 'White', 'Purple'];
+const colorSecondUser = ['Blue', 'Black', 'Grey'];
+const firstUserRestaurant = [
+  'Bento',
+  'Sushi',
+  'Pancake',
+  'Eggy',
+  'Tempura',
+  'Bento',
+  'Eggy',
+  'Padang',
+  'Tteok',
+  'Sushi',
+  'Sushi',
+];
+const secondUserRestaurant = [
+  'Tempura',
+  'Bento',
+  'Sushi',
+  'Pancake',
+  'Padang',
+  'Katsu',
+  'Geprek',
+  'Pancake',
+  'Eggy',
+];
+
+const uniqueFirstUser = new Set(firstUserRestaurant);
+const uniqueSecondUser = new Set(secondUserRestaurant);
+const uniqueColorFirst = new Set(colorFirstUser);
+const uniqueColorSecond = new Set(colorSecondUser);
+
+const firstUser = {
+  name: 'Monica',
+  gender: 'Female',
+  age: 17,
+  email: 'monica@dingdong.com',
+  favoriteColor: [...uniqueColorFirst],
+  isHavePet: 'Yes',
+  education: [
+    {
+      name: 'SD 01',
+      city: 'Jakarta',
+      graduate: 2016,
+    },
+    {
+      name: 'SMP 02',
+      city: 'Jakarta',
+      graduate: 2019,
+    },
+    {
+      name: 'Sma 03',
+      city: 'Tangerang',
+    },
+  ],
+  favoriteRestaurant: [...uniqueFirstUser],
+};
+
+const secondUser = {
+  name: 'Wendy',
+  gender: 'Male',
+  age: 23,
+  email: 'wendy@dingdong.com',
+  favoriteColor: [...uniqueColorSecond],
+  isHavePet: 'No',
+  education: [
+    {
+      name: 'SD 02',
+      city: 'Jakarta',
+      graduate: 2010,
+    },
+    {
+      name: 'SMP 03',
+      city: 'Jakarta',
+      graduate: 2013,
+    },
+    {
+      name: 'SMA 01',
+      city: 'Surabaya',
+      graduate: 2016,
+    },
+
+    {
+      name: 'Universitas Maju',
+      city: 'Surabaya',
+    },
+  ],
+  favoriteRestaurant: [...uniqueSecondUser],
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
